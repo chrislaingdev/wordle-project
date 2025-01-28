f0ea4c3ede8037878917ba87cf3f231ac7ab04ba
Chris Laing <chrislaingdev@gmail.com>
2025-01-27 23:33:19 -0600

    second commit worked on ui a bit

--- wordle-frontend/script.js
+++ wordle-frontend/script.js
@@ -1,10 +1,21 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+let gameActive = false
 const CORRECT_COLOR = "rgb(97, 160, 72)"
 const ALMOST_COLOR = "rgb(207, 192, 53)"
 const WRONG_COLOR = "grey"
 const SQUARE_SIZE = 100;
 const ROW_GAP = 10;
+let currentWordGuess = []
+let firstGuess = ""
+let secondGuess = ""
+let thirdGuess = ""
+let fourthGuess = ""
+let fifthGuess = ""
+let sixthGuess = ""
+let wordsGuessed = 0
+let currentLetterIndex = 0
+
 
 
 
@@ -14,7 +25,7 @@ const ROW_GAP = 10;
 
 
 function drawBoard(){
-  ctx.reset();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   // row 1 
 
   ctx.rect(ROW_GAP,ROW_GAP,SQUARE_SIZE, SQUARE_SIZE);
@@ -66,7 +77,7 @@ function drawBoard(){
 
 function colorSquare(xCor, yCor, color){
 
-  const colors = ["grey", "green", "yellow"]
+  const colors = ["grey", "green", "yellow", "white"]
 
   if (xCor > 4 || yCor > 5 || !colors.includes(color)){
     console.log("one or more parameters are not valid")
@@ -79,11 +90,15 @@ function colorSquare(xCor, yCor, color){
     ctx.fillStyle = CORRECT_COLOR
   } else if (color == "yellow"){
     ctx.fillStyle = ALMOST_COLOR
+  } else if(color == "white"){
+    ctx.fillStyle = "white"
   }
   
 
   if (xCor == 0 && yCor == 0){
     ctx.fillRect(ROW_GAP ,ROW_GAP, SQUARE_SIZE, SQUARE_SIZE);
+    ctx.stroke();
+    ctx.endPath();
   } 
   else{
     if (xCor == 0){
@@ -99,6 +114,9 @@ function colorSquare(xCor, yCor, color){
       yCor =  ROW_GAP * yCor + SQUARE_SIZE * (yCor - 1)
     }
     ctx.fillRect(xCor, yCor,SQUARE_SIZE, SQUARE_SIZE);
+    ctx.stroke();
+    ctx.endPath();
+    
   }
 }
 
@@ -127,19 +145,46 @@ function assignLetter(xCor, yCor, letter, color="white"){
 
 //ctx.stroke();
 
-function newGamePrompt(){
-  ctx.font = "50px Arial";
-  ctx.fillStyle = "black";
-  ctx.fillText("Press enter to start.", 60, 335);
 
-  let enterKey = document.getElementById("enter")
-  //enterKey.addEventListener("click", drawBoard());
 
-  
-  
+
+//function newGamePrompt(){
+//  ctx.font = "50px Arial";
+//  ctx.fillStyle = "black";
+//  ctx.fillText("Press enter to start.", 60, 335);
+//
+//}
+//newGamePrompt()
+
+function guessLetter(letter){
+  if (currentWordGuess.length < 5){
+    assignLetter(currentLetterIndex, wordsGuessed, letter, "black")
+    currentWordGuess.push(letter)
+    currentLetterIndex ++
+  }
 }
-newGamePrompt()
+
+function backspace(){
+  if (currentWordGuess.length > 0){
+    currentWordGuess.pop();
+    currentLetterIndex --
+    colorSquare(currentLetterIndex, wordsGuessed, "white")
+    
+  }
+
+}
+
+function guessWord(){
+  if (currentWordGuess.length == 5){
+    //check to see if words match
+  }
+}
+
+function gameLoop(){
+  drawBoard();
 
 
 
+}
 
+gameLoop()
